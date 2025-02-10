665c6bedfc691f266b2e46623745616c34131b87
Paul Tee <jianhaoti@gmail.com>
2025-02-09 20:35:37 -0500

    page not found

---
+++ src/app/not-found.tsx
@@ -0,0 +1,13 @@
+export default function NotFound() {
+  return (
+    <div className="max-w-2xl mx-auto text-center py-16">
+      <h1 className="text-4xl font-bold   "> Page Not Found</h1>
+      <p className="text-gray-700 mb-8">
+        The page you’re looking for doesn’t exist.
+      </p>
+      <a href="/" className="">
+        Go Back Home
+      </a>
+    </div>
+  );
+}
